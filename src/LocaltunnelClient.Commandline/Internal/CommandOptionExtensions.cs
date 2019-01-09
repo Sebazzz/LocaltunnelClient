009bdafedef4f6180080e46e2f8e1c33e3d403aa
Sebastiaan Dammann <sebastiaandammann@outlook.com>
2019-01-09 21:36:25 +0100

    Designed basic CLI

---
+++ src/LocaltunnelClient.Commandline/Internal/CommandOptionExtensions.cs
@@ -0,0 +1,46 @@
+﻿// ******************************************************************************
+//  © 2019 Sebastiaan Dammann | damsteen.nl
+// 
+//  File:           : CommandOptionExtensions.cs
+//  Project         : LocaltunnelClient.Commandline
+// ******************************************************************************
+
+using System;
+using System.Globalization;
+using Microsoft.Extensions.CommandLineUtils;
+
+namespace LocaltunnelClient.Commandline.Internal
+{
+    internal static class CommandOptionExtensions
+    {
+        public static T Value<T>(this CommandOption commandOption, CommandLineApplication app)
+        {
+            try
+            {
+                return (T) Convert.ChangeType(commandOption.Value(), typeof(T), CultureInfo.InvariantCulture);
+            }
+            catch (Exception ex) when (ex is FormatException || ex is OverflowException)
+            {
+                throw new CommandParsingException(app, $"Invalid option '{commandOption.Value()}'");
+            }
+        }
+
+        public static T RequiredValue<T>(this CommandOption commandOption, CommandLineApplication app)
+        {
+            if (!commandOption.HasValue())
+            {
+                throw new CommandParsingException(app, $"Option '{commandOption.LongName}' is required");
+            }
+
+            return commandOption.Value<T>(app);
+        }
+
+        public static void SetDefault(this CommandOption commandOption, string defaultValue)
+        {
+            if (!commandOption.HasValue())
+            {
+                commandOption.Values.Add(defaultValue);
+            }
+        }
+    }
+}
--- src/LocaltunnelClient.Commandline/Program.cs
+++ src/LocaltunnelClient.Commandline/Program.cs
@@ -5,15 +5,83 @@
 //  Project         : LocaltunnelClient.Commandline
 // ******************************************************************************
 
+using System.Reflection;
+using LocaltunnelClient.Commandline.Internal;
+using Microsoft.Extensions.CommandLineUtils;
+
 namespace LocaltunnelClient.Commandline
 {
     using System;
-
-    class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        private static readonly Assembly ProgramAssembly = typeof(Program).Assembly;
+        private static readonly string ShortVersion = ProgramAssembly.GetName().Version.ToString();
+        private static readonly string LongVersion =  ProgramAssembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion ?? ShortVersion;
+
+        static int Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CommandLineApplication cmdApp = new CommandLineApplication
+            {
+                FullName = "Localtunnel client",
+                Name = "localtunnel",
+                Description = "Expose your local computer",
+                Syntax = "-p [local port] [--server my-own-tunnelserver.net] [--open]"
+            };
+
+            CommandOption serverOption = cmdApp.Option(
+                "--server | -s",
+                "Localtunnel host server - can be a host name or a full URI. In case of a host name, an HTTP connection on default port is attempted.",
+                 CommandOptionType.SingleValue);
+
+            CommandOption localPortOption = cmdApp.Option(
+                "--local-port | -p",
+                "The local port at which incoming traffic is redirected.",
+                CommandOptionType.SingleValue
+            );
+
+            CommandOption hostNameOption = cmdApp.Option(
+                "--domain || -d",
+                "The subdomain of the server to request",
+                CommandOptionType.SingleValue
+            );
+
+            CommandOption openInBrowserOption = cmdApp.Option("--open | -o", "Open the link to the tunnel in the webbrowser",
+                                                    CommandOptionType.NoValue);
+
+
+            cmdApp.VersionOption("--version", ShortVersion, LongVersion);
+
+            cmdApp.HelpOption("-? | -h | --help");
+
+            cmdApp.OnExecute(() =>
+            {
+                serverOption.SetDefault("https://localtunnel.me/");
+
+                string server = serverOption.Value();
+                string subDomain = hostNameOption.Value();
+                int localPort = localPortOption.RequiredValue<int>(cmdApp);
+                bool open = openInBrowserOption.HasValue();
+
+                Console.WriteLine($"Server: {server}");
+                Console.WriteLine($"Sub-domain: {subDomain}");
+                Console.WriteLine($"Port: {localPort}");
+                Console.WriteLine($"Browser: {open}");
+
+                return 0;
+            });
+
+            try
+            {
+                cmdApp.ShowRootCommandFullNameAndVersion();
+
+                return cmdApp.Execute(args);
+            }
+            catch (CommandParsingException ex)
+            {
+                Console.WriteLine(ex.Message);
+
+                return -1;
+            }
         }
     }
 }
